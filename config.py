6c7255675e26d3d71c8449771c6b10ab6fc6f81c
fiapas <maksimgovivok@gmail.com>
2026-08-27 14:02:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
-
 VPN_NAME = "foreveraso VPN"
 BOT_TOKEN = "8917061204:AAGYzu2OVq5NQc8vGwNn4vDrWR94scX7Q7w"
-BOT_USERNAME = "ForeverasoVpn_bot"
 
 ADMIN_IDS = [5926969950, 8293308280]
 CHANNEL_ID = "@Foreveraso_Vpn"
@@ -12,27 +10,11 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGE_PATH = os.path.join(BASE_DIR, "logo.jpg")
 
 FREE_HOURS = 24
+PAID_DAYS = 30
+PAID_PRICE = "300 ₽"
+
 DB_PATH = os.path.join(BASE_DIR, "users.db")
-
-# Тарифы (price_stars – цена в звёздах Telegram)
-TARIFFS = {
-    "free":    {"days": 1,    "label": "Бесплатная",  "price_rub": "0 ₽",   "price_stars": 0,   "pool": "free"},
-    "ref":     {"days": 14,   "label": "Реферальная", "price_rub": "0 ₽",   "price_stars": 0,   "pool": "ref"},
-    "week":    {"days": 7,    "label": "Неделя",      "price_rub": "35 ₽",  "price_stars": 25,  "pool": "week"},
-    "month":   {"days": 30,   "label": "Месяц",       "price_rub": "99 ₽",  "price_stars": 50,  "pool": "month"},
-    "halfyear":{"days": 180,  "label": "Полгода",     "price_rub": "549 ₽", "price_stars": 299, "pool": "halfyear"},
-    "year":    {"days": 365,  "label": "Год",         "price_rub": "999 ₽", "price_stars": 549, "pool": "year"},
-    "forever": {"days": 3650, "label": "Навсегда",    "price_rub": "2499 ₽","price_stars": 1350,"pool": "forever"},
-}
-
-ALL_POOLS = ["free", "ref", "week", "month", "halfyear", "year", "forever"]
-
-POOL_LABELS = {
-    "free":     "Бесплатные",
-    "ref":      "Реферальные",
-    "week":     "Неделя",
-    "month":    "Месяц",
-    "halfyear": "Полгода",
-    "year":     "Год",
-    "forever":  "Навсегда",
-}
+PANEL_URL = "http://89.125.33.130:8443/Cjgo9OTNe2qsekfCkR&quot;
+PANEL_USERNAME = "P4Zo2Vjuot"
+PANEL_PASSWORD = "admin"
+PANEL_INBOUND_ID = 1
